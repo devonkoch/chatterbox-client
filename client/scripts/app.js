e36e940d59f9667f92e691a99cc3f3bc4c024541
devonkoch <devon.r.koch@gmail.com>
2015-07-13 15:47:34 -0700

    displaying 10 most recent messages w/ refresh button

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,33 @@
-// YOUR CODE HERE:
+var data4console;
 
+$(document).ready(function(){
+
+  var getMessages = function(){
+    
+    $('.message-container').empty();
+
+    $.get('https://api.parse.com/1/classes/chatterbox', function(data){
+      
+
+
+      data4console = data;
+
+      last10Messages = data.results.slice(0,10);
+      
+      _.each(last10Messages, function(messageObj){
+        var $divMessage = $('<div class="message"></div>');;
+        $divMessage.text(messageObj.text);
+        $divMessage.appendTo($('.message-container'));
+      });
+
+
+
+    });
+  }
+
+  $('.refresh').on('click', function(){
+    getMessages();
+  });
+
+
+});
